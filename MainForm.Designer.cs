24b1defd1d201f58207504256005282fe3cfe332
Michael Morten Sonne <49366751+michaelmsonne@users.noreply.github.com>
2022-03-26 20:31:03 +0100

    Update GUI

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -76,7 +76,7 @@
             this.btnGenerate.Name = "btnGenerate";
             this.btnGenerate.Size = new System.Drawing.Size(101, 47);
             this.btnGenerate.TabIndex = 26;
-            this.btnGenerate.Text = "Generer";
+            this.btnGenerate.Text = "Generate and copy";
             this.btnGenerate.UseVisualStyleBackColor = false;
             this.btnGenerate.Click += new System.EventHandler(this.BtnGenerate_Click);
             // 
@@ -116,7 +116,7 @@
             // chkRequireSpace
             // 
             this.chkRequireSpace.AutoSize = true;
-            this.chkRequireSpace.Location = new System.Drawing.Point(157, 101);
+            this.chkRequireSpace.Location = new System.Drawing.Point(160, 101);
             this.chkRequireSpace.Name = "chkRequireSpace";
             this.chkRequireSpace.Size = new System.Drawing.Size(15, 14);
             this.chkRequireSpace.TabIndex = 11;
@@ -126,7 +126,7 @@
             // chkRequireUnderscore
             // 
             this.chkRequireUnderscore.AutoSize = true;
-            this.chkRequireUnderscore.Location = new System.Drawing.Point(157, 87);
+            this.chkRequireUnderscore.Location = new System.Drawing.Point(160, 87);
             this.chkRequireUnderscore.Name = "chkRequireUnderscore";
             this.chkRequireUnderscore.Size = new System.Drawing.Size(15, 14);
             this.chkRequireUnderscore.TabIndex = 9;
@@ -139,14 +139,16 @@
             this.txtMaxLength.Name = "txtMaxLength";
             this.txtMaxLength.Size = new System.Drawing.Size(32, 20);
             this.txtMaxLength.TabIndex = 24;
-            this.txtMaxLength.Text = "20";
+            this.txtMaxLength.Text = "25";
             this.txtMaxLength.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             this.txtMaxLength.TextChanged += new System.EventHandler(this.TxtMaxLength_TextChanged);
             // 
             // chkRequireSpecial
             // 
             this.chkRequireSpecial.AutoSize = true;
-            this.chkRequireSpecial.Location = new System.Drawing.Point(157, 73);
+            this.chkRequireSpecial.Checked = true;
+            this.chkRequireSpecial.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkRequireSpecial.Location = new System.Drawing.Point(160, 73);
             this.chkRequireSpecial.Name = "chkRequireSpecial";
             this.chkRequireSpecial.Size = new System.Drawing.Size(15, 14);
             this.chkRequireSpecial.TabIndex = 7;
@@ -158,14 +160,16 @@
             this.Label2.AutoSize = true;
             this.Label2.Location = new System.Drawing.Point(193, 40);
             this.Label2.Name = "Label2";
-            this.Label2.Size = new System.Drawing.Size(73, 13);
+            this.Label2.Size = new System.Drawing.Size(65, 13);
             this.Label2.TabIndex = 30;
-            this.Label2.Text = "Max Længde:";
+            this.Label2.Text = "Max. length:";
             // 
             // chkRequireNumber
             // 
             this.chkRequireNumber.AutoSize = true;
-            this.chkRequireNumber.Location = new System.Drawing.Point(157, 59);
+            this.chkRequireNumber.Checked = true;
+            this.chkRequireNumber.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkRequireNumber.Location = new System.Drawing.Point(160, 59);
             this.chkRequireNumber.Name = "chkRequireNumber";
             this.chkRequireNumber.Size = new System.Drawing.Size(15, 14);
             this.chkRequireNumber.TabIndex = 5;
@@ -178,14 +182,16 @@
             this.txtMinLength.Name = "txtMinLength";
             this.txtMinLength.Size = new System.Drawing.Size(32, 20);
             this.txtMinLength.TabIndex = 23;
-            this.txtMinLength.Text = "14";
+            this.txtMinLength.Text = "16";
             this.txtMinLength.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             this.txtMinLength.TextChanged += new System.EventHandler(this.TxtMinLength_TextChanged);
             // 
             // chkRequireUppercase
             // 
             this.chkRequireUppercase.AutoSize = true;
-            this.chkRequireUppercase.Location = new System.Drawing.Point(157, 45);
+            this.chkRequireUppercase.Checked = true;
+            this.chkRequireUppercase.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkRequireUppercase.Location = new System.Drawing.Point(160, 45);
             this.chkRequireUppercase.Name = "chkRequireUppercase";
             this.chkRequireUppercase.Size = new System.Drawing.Size(15, 14);
             this.chkRequireUppercase.TabIndex = 3;
@@ -197,14 +203,16 @@
             this.Label1.AutoSize = true;
             this.Label1.Location = new System.Drawing.Point(193, 16);
             this.Label1.Name = "Label1";
-            this.Label1.Size = new System.Drawing.Size(70, 13);
+            this.Label1.Size = new System.Drawing.Size(62, 13);
             this.Label1.TabIndex = 29;
-            this.Label1.Text = "Min Længde:";
+            this.Label1.Text = "Min. length:";
             // 
             // chkRequireLowercase
             // 
             this.chkRequireLowercase.AutoSize = true;
-            this.chkRequireLowercase.Location = new System.Drawing.Point(157, 31);
+            this.chkRequireLowercase.Checked = true;
+            this.chkRequireLowercase.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkRequireLowercase.Location = new System.Drawing.Point(160, 31);
             this.chkRequireLowercase.Name = "chkRequireLowercase";
             this.chkRequireLowercase.Size = new System.Drawing.Size(15, 14);
             this.chkRequireLowercase.TabIndex = 1;
@@ -214,7 +222,7 @@
             // chkAllowSpace
             // 
             this.chkAllowSpace.AutoSize = true;
-            this.chkAllowSpace.Location = new System.Drawing.Point(120, 101);
+            this.chkAllowSpace.Location = new System.Drawing.Point(117, 101);
             this.chkAllowSpace.Name = "chkAllowSpace";
             this.chkAllowSpace.Size = new System.Drawing.Size(15, 14);
             this.chkAllowSpace.TabIndex = 10;
@@ -224,7 +232,7 @@
             // chkAllowUnderscore
             // 
             this.chkAllowUnderscore.AutoSize = true;
-            this.chkAllowUnderscore.Location = new System.Drawing.Point(120, 87);
+            this.chkAllowUnderscore.Location = new System.Drawing.Point(117, 87);
             this.chkAllowUnderscore.Name = "chkAllowUnderscore";
             this.chkAllowUnderscore.Size = new System.Drawing.Size(15, 14);
             this.chkAllowUnderscore.TabIndex = 8;
@@ -236,7 +244,7 @@
             this.chkAllowSpecial.AutoSize = true;
             this.chkAllowSpecial.Checked = true;
             this.chkAllowSpecial.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.chkAllowSpecial.Location = new System.Drawing.Point(120, 73);
+            this.chkAllowSpecial.Location = new System.Drawing.Point(117, 73);
             this.chkAllowSpecial.Name = "chkAllowSpecial";
             this.chkAllowSpecial.Size = new System.Drawing.Size(15, 14);
             this.chkAllowSpecial.TabIndex = 6;
@@ -248,7 +256,7 @@
             this.chkAllowNumber.AutoSize = true;
             this.chkAllowNumber.Checked = true;
             this.chkAllowNumber.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.chkAllowNumber.Location = new System.Drawing.Point(120, 59);
+            this.chkAllowNumber.Location = new System.Drawing.Point(117, 59);
             this.chkAllowNumber.Name = "chkAllowNumber";
             this.chkAllowNumber.Size = new System.Drawing.Size(15, 14);
             this.chkAllowNumber.TabIndex = 4;
@@ -260,7 +268,7 @@
             this.chkAllowUppercase.AutoSize = true;
             this.chkAllowUppercase.Checked = true;
             this.chkAllowUppercase.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.chkAllowUppercase.Location = new System.Drawing.Point(120, 45);
+            this.chkAllowUppercase.Location = new System.Drawing.Point(117, 45);
             this.chkAllowUppercase.Name = "chkAllowUppercase";
             this.chkAllowUppercase.Size = new System.Drawing.Size(15, 14);
             this.chkAllowUppercase.TabIndex = 2;
@@ -270,27 +278,27 @@
             // Label10
             // 
             this.Label10.AutoSize = true;
-            this.Label10.Location = new System.Drawing.Point(142, 13);
+            this.Label10.Location = new System.Drawing.Point(145, 13);
             this.Label10.Name = "Label10";
-            this.Label10.Size = new System.Drawing.Size(44, 13);
+            this.Label10.Size = new System.Drawing.Size(45, 13);
             this.Label10.TabIndex = 8;
-            this.Label10.Text = "Kræves";
+            this.Label10.Text = "Needed";
             // 
             // Label9
             // 
             this.Label9.AutoSize = true;
-            this.Label9.Location = new System.Drawing.Point(108, 13);
+            this.Label9.Location = new System.Drawing.Point(105, 13);
             this.Label9.Name = "Label9";
-            this.Label9.Size = new System.Drawing.Size(35, 13);
+            this.Label9.Size = new System.Drawing.Size(44, 13);
             this.Label9.TabIndex = 7;
-            this.Label9.Text = "Tilladt";
+            this.Label9.Text = "Allowed";
             // 
             // chkAllowLowercase
             // 
             this.chkAllowLowercase.AutoSize = true;
             this.chkAllowLowercase.Checked = true;
             this.chkAllowLowercase.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.chkAllowLowercase.Location = new System.Drawing.Point(120, 31);
+            this.chkAllowLowercase.Location = new System.Drawing.Point(117, 31);
             this.chkAllowLowercase.Name = "chkAllowLowercase";
             this.chkAllowLowercase.Size = new System.Drawing.Size(15, 14);
             this.chkAllowLowercase.TabIndex = 0;
@@ -302,9 +310,9 @@
             this.Label8.AutoSize = true;
             this.Label8.Location = new System.Drawing.Point(8, 101);
             this.Label8.Name = "Label8";
-            this.Label8.Size = new System.Drawing.Size(69, 13);
+            this.Label8.Size = new System.Drawing.Size(55, 13);
             this.Label8.TabIndex = 5;
-            this.Label8.Text = "Mellemrum ( )";
+            this.Label8.Text = "Speces ( )";
             // 
             // Label7
             // 
@@ -320,18 +328,18 @@
             this.Label6.AutoSize = true;
             this.Label6.Location = new System.Drawing.Point(8, 73);
             this.Label6.Name = "Label6";
-            this.Label6.Size = new System.Drawing.Size(100, 13);
+            this.Label6.Size = new System.Drawing.Size(94, 13);
             this.Label6.TabIndex = 3;
-            this.Label6.Text = "Special Tegn ($%#)";
+            this.Label6.Text = "Special sign ($%#)";
             // 
             // Label5
             // 
             this.Label5.AutoSize = true;
             this.Label5.Location = new System.Drawing.Point(8, 59);
             this.Label5.Name = "Label5";
-            this.Label5.Size = new System.Drawing.Size(49, 13);
+            this.Label5.Size = new System.Drawing.Size(76, 13);
             this.Label5.TabIndex = 2;
-            this.Label5.Text = "Tal (123)";
+            this.Label5.Text = "Numbers (123)";
             // 
             // Label4
             // 
@@ -351,7 +359,7 @@
             this.Label3.TabIndex = 0;
             this.Label3.Text = "Lowercase (abc)";
             // 
-            // Form1
+            // MainForm
             // 
             this.AcceptButton = this.btnGenerate;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -363,7 +371,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
-            this.Name = "Form1";
+            this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Password Generator";
             this.Load += new System.EventHandler(this.Form1_Load);
